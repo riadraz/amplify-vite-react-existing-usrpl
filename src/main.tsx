aeb5c91c1dec628639f87f6db371958c19edfd83
riadraz <riadraz@gmail.com>
2025-11-16 11:16:04 +0900

    added existing usrpool

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,33 @@ import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "us-east-1_yoZX9xJPN",
+      userPoolClientId: "2pn88vv1jgsuspp50q0vfc9gf1",
+      identityPoolId: "us-east-1:517cc24f-b02d-4754-a7e8-f555cfa6c205",
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+})
+
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
